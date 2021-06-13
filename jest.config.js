61f1eaf019248db9847209c2b2e617e6a4805295
Tim Schmidt <tim.schmidt@dowjones.com>
2021-06-12 21:05:15 -0500

    chore: configuring unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
+  rootDir: 'src/',
   testEnvironment: 'node',
   clearMocks: true,
   resetMocks: true
--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,3 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
 import { LambdaLog } from 'lambda-log';
 import { AwsLambdaLogWrapper } from '../src';
