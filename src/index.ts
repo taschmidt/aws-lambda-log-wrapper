9f43962ebb46ae7a968c63007a3dc2b2349a222b
Tim Schmidt <tim.schmidt@dowjones.com>
2021-06-12 20:38:27 -0500

    chore: truncate deep responses

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,31 @@
 import { Logger } from '@aws-sdk/types';
 import { LambdaLog } from 'lambda-log';
+import truncate from 'json-truncate';
+
+type LogContent = Record<string, unknown>;
 
 export class AwsLambdaLogWrapper implements Logger {
     constructor(public log: LambdaLog) { }
 
-    private contentToParams(content: Record<string, unknown>): [string, Record<string, unknown>] {
+    private contentToParams(content: LogContent): [string, LogContent] {
         const { clientName, commandName } = content;
         content['aws-sdk'] = true;
-        return [`AWS: Call ${clientName}.${commandName}`, content];
+        return [`AWS: Call ${clientName}:${commandName}`, truncate(content, { maxDepth: 3, replace: '[Truncated]' })];
     }
 
-    debug(content: Record<string, unknown>): void {
+    debug(content: LogContent): void {
         this.log.debug(...this.contentToParams(content));
     }
 
-    info(content: Record<string, unknown>): void {
+    info(content: LogContent): void {
         this.log.info(...this.contentToParams(content));
     }
     
-    warn(content: Record<string, unknown>): void {
+    warn(content: LogContent): void {
         this.log.warn(...this.contentToParams(content));
     }
     
-    error(content: Record<string, unknown>): void {
+    error(content: LogContent): void {
         this.log.error(...this.contentToParams(content));
     }
 }
